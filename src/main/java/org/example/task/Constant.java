ed88ec84974e4e3eaf76eb8d5f1972731d4a6bd2
Duranna66 <ivan.kushnarev.04@list.ru>
2023-12-06 14:57:48 +0300

    a

--- src/main/java/org/example/task/Constant.java
+++
@@ -1,6 +0,0 @@
-package org.example.task;
-
-public class Constant {
-    public static final String refOnYanMarket = "https://market.yandex.ru/";
-    public static final  String contentOfSelect = "/html/body/div[5]/div/div/div/div/div/div/div[1]/div/ul/li[6]/a/span";
-}
